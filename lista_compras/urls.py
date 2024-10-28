a85446d1bc6480e70ec2fba0e39fc626e39d2222
CarlosFranchN <carlosaragaoneto@edu.unifor.br>
2024-10-28 20:56:40 -0300

    put action

--- lista_compras/urls.py
+++ lista_compras/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from produtos import views
 
 urlpatterns = [
     path("", include("produtos.url")),
