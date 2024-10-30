4672241313dd08e1f5110b0ad0d363dedea94587
CarlosFranchN <carlosaragaoneto@edu.unifor.br>
2024-10-29 21:12:51 -0300

    Animação nos card de concluido

--- produtos/static/js/categoria.js
+++ produtos/static/js/categoria.js
@@ -1,27 +1,15 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const customSelect = document.querySelector('.custom-select');
-    const header = customSelect.querySelector('.select-header');
-    const optionsList = customSelect.querySelector('.options-list');
-    const hiddenInput = customSelect.querySelector('input[type="hidden"]');
-    
-    header.addEventListener('click', function() {
-        optionsList.style.display = optionsList.style.display === 'none' ? 'block' : 'none';
-    });
-    
-    const options = customSelect.querySelectorAll('.option');
-    options.forEach(option => {
-        option.addEventListener('click', function() {
-            const value = this.dataset.value;
-            const text = this.querySelector('span').textContent;
-            header.querySelector('.selected-option').textContent = text;
-            hiddenInput.value = value;
-            optionsList.style.display = 'none';
-        });
-    });
-    
-    document.addEventListener('click', function(e) {
-        if (!customSelect.contains(e.target)) {
-            optionsList.style.display = 'none';
-        }
-    });
-});
+function check() {
+    // Obtém todos os checkboxes com a classe 'item'
+    const checkbox = document.querySelector(".checkbox_item");
+    const pai = checkbox.parentNode.parentNode.parentNode
+    console.log(checkbox);
+    console.log(pai);
+      if (checkbox.checked) {
+        // Adiciona a classe 'strikethrough' ao nome do item e à caixa
+        pai.classList.add("strikethrough");
+      } else {
+        // Remove a classe 'strikethrough' se o checkbox for desmarcado
+        pai.classList.remove("strikethrough");
+      }
+
+    }
